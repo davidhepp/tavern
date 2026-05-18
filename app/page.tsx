8f7f020ec958bc286451ca59574d9e8b1ffca0fa
David Heppenheimer <david@heppify.de>
2026-05-18 22:23:25 +0200

    feat: implement loading spinner and error state for session authentication in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,33 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
-  const { data: session } = authClient.useSession();
+  const { data: session, isPending, error } = authClient.useSession();
+
+  if (isPending) {
+    return (
+      <div className="flex h-dvh w-dvw items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex h-dvh w-dvw items-center justify-center">
+        <p>Something went wrong.</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       {session ? (
         <div>
-          <Button onClick={() => authClient.signOut({})}>Sign out</Button>
+          <Button onClick={() => authClient.signOut()}>Sign out</Button>
           <h1>Hello {session.user.name}</h1>
         </div>
       ) : (
