ded2314e5ea1d5d6e639a460f19c45d60b9738c9
David Heppenheimer <david@heppify.de>
2026-05-18 22:03:54 +0200

    feat: added and initted drizzle with auth schema from better-auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,10 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
+import { db } from "./db";
 
 export const auth = betterAuth({
-  //...
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "pg" or "mysql"
+  }),
+  //... the rest of your config
 });
