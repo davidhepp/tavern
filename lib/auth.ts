f3b4fa15938d1aae245e5763f4ffe78c8fd1740d
David Heppenheimer <david@heppify.de>
2026-05-18 22:07:01 +0200

    feat: enabled github oauth and email + password signin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,18 @@
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "@better-auth/drizzle-adapter";
-import { db } from "./db";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/lib/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg", // or "pg" or "mysql"
+    provider: "pg",
   }),
-  //... the rest of your config
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
 });
