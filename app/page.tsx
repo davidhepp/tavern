aab1bf4b968d9e08bbd603d179de6300268b5725
David Heppenheimer <david@heppify.de>
2026-05-18 22:20:37 +0200

    feat: integrate better-auth with github oauth and session management on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 export default function Home() {
-  return <div>Hello World</div>;
+  const { data: session } = authClient.useSession();
+  return (
+    <div>
+      {session ? (
+        <div>
+          <Button onClick={() => authClient.signOut({})}>Sign out</Button>
+          <h1>Hello {session.user.name}</h1>
+        </div>
+      ) : (
+        <Button
+          onClick={() =>
+            authClient.signIn.social({
+              provider: "github",
+            })
+          }
+        >
+          Sign in with Github
+        </Button>
+      )}
+    </div>
+  );
 }
